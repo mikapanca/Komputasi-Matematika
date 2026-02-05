9cb5e0a0680f7505df546428da6956d0ff7dc424
Mika Panca Okta Ariandi <mikapanca25@gmail.com>
2026-02-05 15:34:34 +0700

    Add files via upload

---
+++ Genetic Algorithm/main.m
@@ -0,0 +1,28 @@
+clc
+clear
+%gen
+target = 'Mika_Panca'
+len = length(target);
+genes = create_genes(len);
+
+%fitnes
+fitness = calculate_fitness(genes,len);
+
+%population
+population_size = 10;
+population = create_population(target,population_size)
+
+%selection
+[parent1, parent2] = selection(population)
+
+%crossover
+[child1, child2] = crossover(parent1,parent2)
+
+%mutation
+mutation_rate=0.5;
+mutant1= mutation(child1, mutation_rate)
+mutant2= mutation(child2, mutation_rate)
+
+%regeneration
+children = [mutant1, mutant2];
+population = regeneration(children, population)
